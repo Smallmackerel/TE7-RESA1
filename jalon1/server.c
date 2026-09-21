ac9ac651646d956fd3d8caf398371ea32479affb
HAQUIN Jean <jhaquin@bordeaux-inp.fr>
2026-09-21 15:56:00 +0200

    tâche 1.1, 1.2 sans la gestion multiclient

---
+++ jalon1/server.c
@@ -0,0 +1,90 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "common.h"
+
+void echo_server(int sockfd) {
+	char buff[MSG_LEN];
+	while (1) {
+		// Cleaning memory
+		memset(buff, 0, MSG_LEN);
+		// Receiving message
+		if (recv(sockfd, buff, MSG_LEN, 0) <= 0) {
+			break;
+		}
+		printf("Received: %s", buff);
+		// Sending message (ECHO)
+		if (send(sockfd, buff, strlen(buff), 0) <= 0) {
+			break;
+		}
+		printf("Message sent!\n");
+	}
+}
+
+int handle_bind(char* serv_port) {
+    char* s_port;
+    if(serv_port != NULL){
+        s_port = serv_port;
+    }
+    else{
+        s_port = SERV_PORT;
+    }
+	struct addrinfo hints, *result, *rp;
+	int sfd;
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+	if (getaddrinfo(NULL, s_port, &hints, &result) != 0) {
+		perror("getaddrinfo()");
+		exit(EXIT_FAILURE);
+	}
+	for (rp = result; rp != NULL; rp = rp->ai_next) {
+		sfd = socket(rp->ai_family, rp->ai_socktype,
+		rp->ai_protocol);
+		if (sfd == -1) {
+			continue;
+		}
+		if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0) {
+			break;
+		}
+		close(sfd);
+	}
+	if (rp == NULL) {
+		fprintf(stderr, "Could not bind\n");
+		exit(EXIT_FAILURE);
+	}
+	freeaddrinfo(result);
+	return sfd;
+}
+
+int main(int argc, char* argv[]) {
+	struct sockaddr cli;
+	int sfd, connfd;
+	socklen_t len;
+    if(argc == 2){
+	    sfd = handle_bind(argv[1]);
+    }
+    else{
+        sfd = handle_bind(NULL);
+    }
+        if ((listen(sfd, SOMAXCONN)) != 0) {
+		perror("listen()\n");
+		exit(EXIT_FAILURE);
+	}
+	len = sizeof(cli);
+	if ((connfd = accept(sfd, (struct sockaddr*) &cli, &len)) < 0) {
+		perror("accept()\n");
+		exit(EXIT_FAILURE);
+	}
+	echo_server(connfd);
+	close(sfd);
+	return EXIT_SUCCESS;
+}
+
